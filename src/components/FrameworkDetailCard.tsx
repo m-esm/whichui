ae8467046a820a5000c15e128f587ee8b9445b07
Mohsen Esmaeili <mohsen.esmaeili@starship.co>
2024-04-01 01:47:08 +0300

    fix detail card

--- src/components/FrameworkDetailCard.tsx
+++ src/components/FrameworkDetailCard.tsx
@@ -16,44 +16,44 @@ export const FrameworkDetailCard = ({
     docs: string;
   };
 }) => (
-  <div className="mb-4 p-4 rounded-lg shadow-lg bg-blue-500 text-orange-500">
+  <div className="mb-4 p-4 rounded-lg shadow-lg bg-white">
     <div className="flex items-center justify-between mb-4">
-      <h3 className="text-lg font-semibold text-blue-900">{name}</h3>
+      <h3 className="text-lg font-semibold ">{name}</h3>
       {frameworkDetails?.[name]?.logo && (
         <img src={frameworkDetails[name].logo} alt={name} className="h-6 w-6" />
       )}
     </div>
-    <p className="text-blue-900">
+    <p className="">
       <strong>Configuration Methods:</strong> {details.configurationMethods}
     </p>
-    <p className="text-blue-900">
+    <p className="">
       <strong>Special Installation Requirements:</strong>{" "}
       {details.specialInstallationRequirements}
     </p>
-    <p className="text-blue-900">
+    <p className="">
       <strong>License:</strong> {details.license}
     </p>
-    <p className="text-blue-900">
+    <p className="">
       <strong>Pricing:</strong> {details.pricing}
     </p>
-    <p className="text-blue-900">
+    <p className="">
       <strong>RTL Support:</strong> {details.RTLsupport}
     </p>
-    <p className="text-blue-900">
+    <p className="">
       <strong>Github:</strong>
-      <a href={details.github} className="text-green-500">
+      <a href={details.github} className="">
         {details.github}
       </a>
     </p>
-    <p className="text-blue-900">
+    <p className="">
       <strong>Website:</strong>
-      <a href={details.website} className="text-green-500">
+      <a href={details.website} className="">
         {details.website}
       </a>
     </p>
-    <p className="text-blue-900">
+    <p className="">
       <strong>Docs:</strong>
-      <a href={details.docs} className="text-green-500">
+      <a href={details.docs} className="">
         {details.docs}
       </a>
     </p>
