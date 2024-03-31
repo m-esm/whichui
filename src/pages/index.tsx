2a56a154517ab124a901ee9b594760e686c53e2f
Mohsen Esmaeili <mohsen.esmaeili@starship.co>
2024-04-01 00:17:16 +0300

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,248 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { useEffect, useState, ReactNode, FC } from "react";
+import {
+  UiComponents,
+  UiFrameworks,
+  antDesignSupportedComponents,
+  chakraUiSupportedComponents,
+  frameworkDetails,
+  materialUiSupportedComponents,
+  nextUiSupportedComponents,
+  radixUiSupportedComponents,
+  shadcnUiSupportedComponents,
+} from "../lib/data";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ["latin"] });
+interface TableProps {
+  children: ReactNode;
+  className?: string;
+}
+
+interface TableHeadProps {
+  isSticky: boolean;
+  children: ReactNode;
+}
+
+interface TableRowProps {
+  children: ReactNode;
+}
+
+interface TableHeaderProps {
+  children: ReactNode;
+}
+
+interface TableBodyProps {
+  children: ReactNode;
+}
+
+interface TableCellProps {
+  children: ReactNode;
+}
+
+const Table: FC<TableProps> = ({ children, className = "" }) => (
+  <table className={`min-w-full divide-y divide-gray-200 ${className}`}>
+    {children}
+  </table>
+);
+
+const TableHead: FC<TableHeadProps> = ({ isSticky, children }) => (
+  <thead
+    className={`${
+      isSticky ? "sticky top-0 z-10 bg-white" : ""
+    } text-xs font-medium uppercase tracking-wider text-gray-700`}
+  >
+    {children}
+  </thead>
+);
+
+const TableRow: FC<TableRowProps> = ({ children }) => <tr>{children}</tr>;
+
+const TableHeader: FC<TableHeaderProps> = ({ children }) => (
+  <th className="px-6 py-3 text-left">{children}</th>
+);
+
+const TableBody: FC<TableBodyProps> = ({ children }) => (
+  <tbody className="bg-white divide-y divide-gray-200">{children}</tbody>
+);
+
+const TableCell: FC<TableCellProps> = ({ children }) => (
+  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+    {children}
+  </td>
+);
+
+const colorPalette = {
+  orange: "#FF6700",
+  lightGray: "#EBEBEB",
+  gray: "#C0C0C0",
+  lightBlue: "#3A6EA5",
+  darkBlue: "#004E98",
+  green: "#16A173",
+};
+
+const FrameworkDetailCard = ({
+  name,
+  details,
+}: {
+  name: string;
+  details: {
+    configurationMethods: string;
+    specialInstallationRequirements: string;
+    license: string;
+    pricing: string;
+    RTLsupport: string;
+  };
+}) => (
+  <div
+    className="mb-4 p-4 rounded-lg shadow-lg"
+    style={{
+      backgroundColor: colorPalette.lightBlue,
+      color: colorPalette.orange,
+    }}
+  >
+    <h3 className="text-lg font-semibold">{name}</h3>
+    <p>
+      <strong>Configuration Methods:</strong> {details.configurationMethods}
+    </p>
+    <p>
+      <strong>Special Installation Requirements:</strong>{" "}
+      {details.specialInstallationRequirements}
+    </p>
+    <p>
+      <strong>License:</strong> {details.license}
+    </p>
+    <p>
+      <strong>Pricing:</strong> {details.pricing}
+    </p>
+    <p>
+      <strong>RTL Support:</strong> {details.RTLsupport}
+    </p>
+  </div>
+);
 
 export default function Home() {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsSticky(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
         />
-      </div>
+      </Head>
+      <main
+        className="p-4 md:p-12 min-h-screen"
+        style={{
+          color: colorPalette.darkBlue,
+          backgroundColor: colorPalette.lightGray,
+        }}
+      >
+        <div className="overflow-x-auto">
+          <Table className="w-full">
+            <TableHead isSticky={isSticky}>
+              <TableRow>
+                <TableHeader>Component</TableHeader>
+                {UiFrameworks.map((framework) => {
+                  const supportedComponents = Object.keys(frameworkDetails[framework]).length;
+                  const totalComponents = UiComponents.length;
+                  const supportedComponentsCount = `${supportedComponents}/${totalComponents}`;
+                  return (
+                    <TableHeader key={framework}>
+                      {frameworkDetails?.[framework]?.logo && (
+                        <img
+                          src={frameworkDetails[framework].logo}
+                          alt={framework}
+                          className="h-6 w-6 inline-block mr-2"
+                        />
+                      )}
+                      {`${framework} (${supportedComponentsCount})`}
+                    </TableHeader>
+                  );
+                })}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {UiComponents.map((component) => (
+                <TableRow key={component}>
+                  <TableCell>{component}</TableCell>
+                  {UiFrameworks.map((framework) => (
+                    <TableCell key={framework}>
+                      {nextUiSupportedComponents[component] &&
+                      framework === "NextUI" ? (
+                        <i className="fas fa-check text-green-500"></i>
+                      ) : radixUiSupportedComponents[component] &&
+                        framework === "RadixUI" ? (
+                        <i className="fas fa-check text-green-500"></i>
+                      ) : shadcnUiSupportedComponents[component] &&
+                        framework === "ShadcnUI" ? (
+                        <i className="fas fa-check text-green-500"></i>
+                      ) : materialUiSupportedComponents[component] &&
+                        framework === "MaterialUI" ? (
+                        <i className="fas fa-check text-green-500"></i>
+                      ) : chakraUiSupportedComponents[component] &&
+                        framework === "ChakraUI" ? (
+                        <i className="fas fa-check text-green-500"></i>
+                      ) : antDesignSupportedComponents[component] &&
+                        framework === "AntDesign" ? (
+                        <i className="fas fa-check text-green-500"></i>
+                      ) : (
+                        <i className="fas fa-times text-red-500"></i>
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <hr className="my-8" />
+        <div className="block md:hidden">
+          {Object.entries(frameworkDetails).map(([name, details]) => (
+            <FrameworkDetailCard key={name} name={name} details={details} />
+          ))}
+        </div>
+        <div className="hidden md:block overflow-x-auto">
+          <Table className="w-full">
+            <TableHead isSticky={isSticky}>
+              <TableRow>
+                <TableHeader>Framework</TableHeader>
+                <TableHeader>Configuration Methods</TableHeader>
+                <TableHeader>Special Installation Requirements</TableHeader>
+                <TableHeader>License</TableHeader>
+                <TableHeader>Pricing</TableHeader>
+                <TableHeader>RTL Support</TableHeader>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Object.entries(frameworkDetails).map(([name, details]) => (
+                <TableRow key={name}>
+                  <TableCell>{name}</TableCell>
+                  <TableCell>{details.configurationMethods}</TableCell>
+                  <TableCell>
+                    {details.specialInstallationRequirements}
+                  </TableCell>
+                  <TableCell>{details.license}</TableCell>
+                  <TableCell>{details.pricing}</TableCell>
+                  <TableCell>{details.RTLsupport}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </main>
+    </>
   );
 }
